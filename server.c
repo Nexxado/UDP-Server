2a6b54128d57d0259fdfc1b7b47112b074817111
ND88 Vivobook Ubuntu <ndraiman@gmail.com>
2016-01-19 23:44:35 +0200

    initial commit

---
+++ server.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netdb.h>
+
+
+int main() {
+
+        int fd;
+        if((fd = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
+                
+        }
+
+}
